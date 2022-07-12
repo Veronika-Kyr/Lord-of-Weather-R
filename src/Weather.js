c68bd6ca96f1bf9b34a34fdfedd0cf1297ceb723
Veronika-Kyr <106926961+Veronika-Kyr@users.noreply.github.com>
2022-07-12 16:48:38 +0300

    Add searching current weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
-import CitySearch from "./CitySearch";
 
-export default function Weather() {
-    let weatherData = {
-        city: "Kyiv",
-        date: "May 27",
-        imgUrl: "https://openweathermap.org/img/wn/01d@2x.png",
-        currentTemperature: 28,
-        maxTemperature: 30,
-        minTemperature: 19,
-        humidity: 40,
-        wind: 4,
-        skyDescription: "sunny"
-    };
+export default function Weather(props) {
+    let [city, setCity] = useState(props.city);
+    let date = "May 27";
+    let [currentWeather, setCurrentWeather] = useState({});
+    function getCity(event) {
+        setCity(event.target.value);
+    }
+    function callCurrentWeather(event) {
+        event.preventDefault();
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=936d2722fd6ddb5c6ab52ceb5b0238af&units=metric`;
+        axios.get(url).then(getCurrentWeather);
+    }
+    function getCurrentWeather(response) {
+        setCurrentWeather({
+            temperature: Math.round(response.data.main.temp),
+            windSpeed: Math.round(response.data.wind.speed),
+            humidity: response.data.main.humidity,
+            description: response.data.weather[0].description,
+            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            maxTemperature: Math.round(response.data.main.temp_max),
+            minTemperature: Math.round(response.data.main.temp_min),
+        })
+    }
     return (
         <div className="Weather">
             <div className="block">
                 <div class="block1">
                     <h4>🌈Lord of Weather</h4>
 
-                    <p className="head">{weatherData.city}</p>
+                    <p className="head">{city}</p>
 
                     <div className="container">
                         <div className="row">
                             <div className="col currentBlock">
                                 <div className="row">
                                     <div className="col-2 col-sm bigdate">
-                                        <p>{weatherData.date}</p>
+                                        <p>{date}</p>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-12 col-md-auto">
                                         <img
-                                            src={weatherData.imgUrl}
-                                            alt={weatherData.skyDescription}
+                                            src={currentWeather.icon}
+                                            alt={currentWeather.description}
                                             className="skyIcon"
                                         />
                                         <span className="daytemp">
-                                            {weatherData.currentTemperature}
+                                            {currentWeather.temperature}
                                         </span>
                                         <a href="/" class="measureC" rel="noreferrer">
                                             ℃
@@ -49,11 +60,11 @@ export default function Weather() {
                                     <div className="col-8 col-md-auto">
                                         <div className="minmaxTemp">
                                             <span className="maxTemp">
-                                                {weatherData.maxTemperature}
+                                                {currentWeather.maxTemperature}
                                             </span>{" "}
                                             <span className="slash">/</span>
                                             <span className="nighttemp">
-                                                {weatherData.minTemperature}
+                                                {currentWeather.minTemperature}
                                             </span>
                                             <a href="/" className="measureC celciusmin" rel="noreferrer">
                                                 ℃
@@ -63,14 +74,14 @@ export default function Weather() {
                                     <div className="col-2 col-sm hum">
                                         <div className="detailed">
                                             <i className="fa-solid fa-droplet bigdrop"></i>
-                                            <span> {weatherData.humidity}</span> %
+                                            <span> {currentWeather.humidity}</span> %
                                             <br />
                                             <i className="fa-solid fa-wind bigwind"></i>
-                                            <span> {weatherData.wind} </span> km/h
+                                            <span> {currentWeather.windSpeed} </span> km/h
                                             <br />
                                             <span className="weatherDescription">
                                                 {" "}
-                                                {weatherData.skyDescription}{" "}
+                                                {currentWeather.description}{" "}
                                             </span>
                                         </div>
                                     </div>
@@ -79,7 +90,17 @@ export default function Weather() {
                         </div>
                     </div>
 
-                    <CitySearch />
+                    <div className="CitySearch">
+                        <form onSubmit={callCurrentWeather}>
+                            <input
+                                className="city-search"
+                                type="search"
+                                placeholder="Search for a city" onChange={getCity}
+                            />
+                            <input className="btn-search" type="submit" value="GO 🔎" />
+                            <button className="showCurrent">Current</button>
+                        </form>
+                    </div>
                 </div>
             </div>
             <div className="gitRef">
@@ -87,7 +108,7 @@ export default function Weather() {
                     {" "}
                     <a
                         href="https://github.com/Veronika-Kyr/Lord-of-Weather-R"
-                        target="_blank" rel="noopener"
+                        target="_blank" rel="noreferrer"
                     >
                         Open-source code
                     </a>{" "}
